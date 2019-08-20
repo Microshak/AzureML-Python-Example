b2eec81a50ffd2cf8f153705ffac7fce41ce6db2
Your Name <>
2019-08-20 10:58:31 -0700

    First

---
+++ 1Train.py
@@ -0,0 +1,46 @@
+import os
+from azureml.core.authentication import ServicePrincipalAuthentication
+from azureml.core import Workspace
+import AML 
+aml = AML.AML()
+ws = aml.ws
+
+   
+
+print("Found workspace {} at location {}".format(ws.name, ws.location))
+
+
+from azureml.core import Experiment
+experiment = Experiment(workspace=ws, name="diabetes-experiment")
+
+
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+
+X, y = load_diabetes(return_X_y = True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=66)
+
+from sklearn.linear_model import Ridge
+from sklearn.metrics import mean_squared_error
+from sklearn.externals import joblib
+import math
+
+alphas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+
+for alpha in alphas:
+    run = experiment.start_logging()
+    run.log("alpha_value", alpha)
+
+    model = Ridge(alpha=alpha)
+    model.fit(X=X_train, y=y_train)
+    y_pred = model.predict(X=X_test)
+    rmse = math.sqrt(mean_squared_error(y_true=y_test, y_pred=y_pred))
+    run.log("rmse", rmse)
+
+    model_name = "model_alpha_" + str(alpha) + ".pkl"
+    filename = "outputs/" + model_name
+
+    joblib.dump(value=model, filename=filename)
+    run.upload_file(name=model_name, path_or_stream=filename)
+    run.complete()
+
