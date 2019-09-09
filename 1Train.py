e3f4783adefed42a04844d84ccc1150be45c420a
unknown <roshak@hotmail.com>
2019-09-09 11:09:00 -0700

    reord

--- 1Train.py
+++ 1Train.py
@@ -2,28 +2,24 @@ import os
 from azureml.core.authentication import ServicePrincipalAuthentication
 from azureml.core import Workspace
 import AML 
-aml = AML.AML()
-ws = aml.ws
-
-   
-
-print("Found workspace {} at location {}".format(ws.name, ws.location))
-
-
 from azureml.core import Experiment
-experiment = Experiment(workspace=ws, name="diabetes-experiment")
-
-
 from sklearn.datasets import load_diabetes
 from sklearn.model_selection import train_test_split
-
-X, y = load_diabetes(return_X_y = True)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=66)
-
 from sklearn.linear_model import Ridge
 from sklearn.metrics import mean_squared_error
 from sklearn.externals import joblib
 import math
+
+aml = AML.AML()
+ws = aml.ws
+
+print("Found workspace {} at location {}".format(ws.name, ws.location))
+
+experiment = Experiment(workspace=ws, name="diabetes-experiment")
+
+X, y = load_diabetes(return_X_y = True)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=66)
+
 
 alphas = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
 
